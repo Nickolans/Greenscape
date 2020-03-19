ba03826afad61440a9ef979d5021413c2695af6e
Nick Griffith <nickolansgriffith@gmail.com>
2020-03-18 21:31:49 -0500

    Initial Commit

---
+++ Greenscape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Greenscape
+//
+//  Created by Nickolans Griffith on 3/18/20.
+//  Copyright © 2020 Nickolans Griffith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
